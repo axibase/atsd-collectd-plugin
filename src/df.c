302254fdcfb296451320767e122fe97520f78298
Rustam Makulov <rmakulov@users.noreply.github.com>
2016-05-27 13:37:39 +0300

    Update df.c

--- src/df.c
+++ src/df.c
@@ -55,7 +55,8 @@ static const char *config_keys[] =
                 "ReportByDevice",
                 "ReportInodes",
                 "ValuesAbsolute",
-                "ValuesPercentage"
+                "ValuesPercentage",
+                "DiscardPrefix"
         };
 static int config_keys_num = STATIC_ARRAY_SIZE(config_keys);
 
