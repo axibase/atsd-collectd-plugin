77052497e1e9c3f1ffb142ba545247bdda61c8c8
rmakulov <rustam.makulov@axibase.com>
2016-11-14 09:58:20 +0300

    updated exec handling

--- src/write_atsd.c
+++ src/write_atsd.c
@@ -922,8 +922,8 @@ static int wa_write_messages(const data_set_t *ds, const value_list_t *vl,
             };
             continue;
         }
-        else if (starts_with("lvs.", vl->plugin)) {
-            strlcat(metric_name, vl->plugin, sizeof(metric_name));
+        else if (strcasecmp(vl->plugin, "exec") == 0) {
+            strlcat(metric_name, vl->plugin_instance, sizeof(metric_name));
             if (cb->wa_num_caches > 0) {
                 ak = (atsd_key_t *) malloc(sizeof(*ak));
                 if (ak == NULL) {
@@ -951,8 +951,19 @@ static int wa_write_messages(const data_set_t *ds, const value_list_t *vl,
                 pthread_mutex_unlock(&cb->avl_lock);
             }
             if (same_value > 0) {
-                ssnprintf(sendline, sizeof(sendline), "series e:%s ms:%" PRIu64 " m:%s=%s t:volume_group=%s t:logical_volume=%s\n",
-                        entity, CDTIME_T_TO_MS(vl->time), metric_name, ret, vl->plugin_instance, vl->type_instance);
+                ssnprintf(sendline, sizeof(sendline), "series e:%s ms:%" PRIu64 " m:%s=%s", entity, CDTIME_T_TO_MS(vl->time), metric_name, ret);
+
+                char* key_value = strtok(vl->type_instance, ";");
+                while (key_value != NULL) {
+                    if (key_value != NULL) {
+                        strlcat(sendline, " t:", sizeof(sendline));
+                        strlcat(sendline, key_value, sizeof(sendline));
+                    }
+                    key_value = strtok(NULL, ";=");
+                }
+                sfree(key_value);
+
+                strlcat(sendline, "\n", sizeof(sendline));
                 status = wa_send_message(sendline, cb);
                 if (status != 0){
                     sfree(rates);
